517eb8f211a389a5951078dcbdc3a902585211f5
Anton <antonmenc@gmail.com>
2020-05-05 00:37:18 +0300

    admin.py

--- django_movie/admin.py
+++ django_movie/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (
+    RatingStar,
+    Rating,
+    Review,
+    Actors,
+    Category,
+    MovieShots,
+    Movie,
+    Genre,
+)
+
+admin.site.register(RatingStar)
+admin.site.register(Rating)
+admin.site.register(Review)
+admin.site.register(Actors)
+admin.site.register(Category)
+admin.site.register(MovieShots)
+admin.site.register(Movie)
+admin.site.register(Genre)
