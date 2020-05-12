edf4370b428986600ae96675b01dd19f80c38be8
Anton <antonmenc@gmail.com>
2020-05-12 23:39:11 +0300

    customize admin.py

--- django_movie/admin.py
+++ django_movie/admin.py
@@ -12,11 +12,36 @@ from .models import (
     Genre,
 )
 
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'url')
+    list_display_links = ['name']
+
+
+class ReviewInLine(admin.StackedInline):
+    model = Review
+    extra = 1
+    readonly_fields = ['name', 'email']
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ['category', 'year']
+    search_fields = ['title', 'category__name']
+    inlines = [ReviewInLine]
+    save_on_top = True
+    save_as = True
+    list_editable = ['draft']
+
+@admin.register(Review)
+class AdminReview(admin.ModelAdmin):
+    list_display = ['name', 'email', 'parent', 'movie', 'id']
+    readonly_fields = ['name', 'email']
+
+
 admin.site.register(RatingStar)
 admin.site.register(Rating)
-admin.site.register(Review)
 admin.site.register(Actors)
-admin.site.register(Category)
 admin.site.register(MovieShots)
-admin.site.register(Movie)
 admin.site.register(Genre)
